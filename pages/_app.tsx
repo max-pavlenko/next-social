387c203f72c98fd3e19e72ab3352f2b3e8ffaf73
exe.cute <90452860+Max211922@users.noreply.github.com>
2022-10-04 15:35:47 +0300

    Final. TODO: comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,38 @@
 import '../styles/globals.css'
+import '../styles/nprogress.css'
+import NavBar from '../components/layout/NavBar';
+import { Toaster } from 'react-hot-toast';
+import { ThemeProvider } from '@mui/material';
+import { useUserData } from '../libs/hooks/useUserData';
+import { theme } from '../utils/constants';
+import NProgress from 'nprogress';
+import { Router, useRouter } from 'next/router';
+import { ContextMenu } from '../components/utils/contextMenu';
+import Loader from '../components/layout/Loader';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+NProgress.configure({showSpinner: true});
+Router.events.on('routeChangeStart', NProgress.start);
+
+Router.events.on('routeChangeError', NProgress.done);
+
+Router.events.on('routeChangeComplete', NProgress.done)
+
+function MyApp({Component, pageProps}) {
+   const {route} = useRouter();
+
+   const {isAuthenticating} = useUserData();
+
+   if (isAuthenticating) return <Loader/>;
+   console.log('isAuthenticating', isAuthenticating)
+
+   return <ThemeProvider theme = {theme}>
+
+             <NavBar/>
+             <Component key = {route} {...pageProps} />
+
+      <Toaster/>
+      <ContextMenu/>
+   </ThemeProvider>
 }
 
 export default MyApp
