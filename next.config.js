5c7ff3d5d9dd44800259b707a25403e9d13278ec
ex3-cut3 <pavlienko1112@gmail.com>
2022-12-07 23:27:24 +0200

    next.config update 2

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  reactStrictMode: false,
-  swcMinify: true,
-  ignoreBuildErrors: true,
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    domains: ['lh3.googleusercontent.com', 'cdn.pixabay.com', 'cdn-icons-png.flaticon.com', 'firebasestorage.googleapis.com', 'upload.wikimedia.org']
-  },
-  i18n: {
-    locales: ['en', 'ua', 'ru'],
-    defaultLocale: 'en'
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    reactStrictMode: false,
+    swcMinify: true,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        domains: ['lh3.googleusercontent.com', 'cdn.pixabay.com', 'cdn-icons-png.flaticon.com', 'firebasestorage.googleapis.com', 'upload.wikimedia.org']
+    },
+    i18n: {
+        locales: ['en', 'ua', 'ru'],
+        defaultLocale: 'en'
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
